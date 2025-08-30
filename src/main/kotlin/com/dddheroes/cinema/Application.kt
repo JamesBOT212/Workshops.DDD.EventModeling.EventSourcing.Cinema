338c7f795cb9cb0e3e5934bdd325579c684bc521
Mateusz Nowak <mateusz@nakodach.pl>
2025-08-30 12:40:12 +0200

    🎉 chore: initialize Spring Boot project

---
+++ src/main/kotlin/com/dddheroes/cinema/Application.kt
@@ -0,0 +1,11 @@
+package com.dddheroes.cinema
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
---
+++ src/test/kotlin/com/dddheroes/cinema/TestcontainersConfiguration.kt
@@ -0,0 +1,6 @@
+package com.dddheroes.cinema
+
+import org.springframework.boot.test.context.TestConfiguration
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration
